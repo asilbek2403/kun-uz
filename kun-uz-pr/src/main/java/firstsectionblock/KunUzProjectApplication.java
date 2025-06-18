7c245cfa54bab70792a9de6882bcb437ccbe3041
Asilbek <asilbeksafarov962@gmail.com>
2025-06-18 10:55:11 +0500

    Project stajor kun.uz backend

---
+++ kun-uz-pr/src/main/java/firstsectionblock/KunUzProjectApplication.java
@@ -0,0 +1,13 @@
+package firstsectionblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunUzProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunUzProjectApplication.class, args);
+	}
+
+}
---
+++ kun-uz-pr/src/test/java/firstsectionblock/KunUzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package firstsectionblock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
