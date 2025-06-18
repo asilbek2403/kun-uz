c61fb23957f940390bb1eb5b21cfe5474a3f62d8
Asilbek <asilbeksafarov962@gmail.com>
2025-06-18 12:38:32 +0500

    getter and setter(Section__)

---
+++ src/main/java/SectionController.java
@@ -0,0 +1,12 @@
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+@Setter
+@RestController
+@RequestMapping("/section")
+public class SectionController {
+
+}
